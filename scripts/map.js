8eb5a0d5abd9d474a7817b247c7e3c2f0b64a78b
gisvtvt <89518118+gisvtvt@users.noreply.github.com>
2021-08-25 09:55:08 -0400

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -128,7 +128,7 @@ $(window).on('load', function() {
       if (point.Latitude !== '' && point.Longitude !== '') {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
           .bindPopup("<b>" + point['Name'] + '</b><br>' +
-          (point['Image'] ? ('<IFRAME src="' + point['Image'] + '"></iframe><br>') : '') +
+          (point['Image'] ? ('<IFRAME src="' + point['Image'] + '"><br>') : '') +
           point['Description']);
 
         if (layers !== undefined && layers.length !== 1) {
